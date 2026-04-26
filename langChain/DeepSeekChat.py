d8ce07b4a190f6676c1eb1be0d89c636a4b79096
tianyipeng666 <506575523@qq.com>
2026-04-26 17:55:31 +0800

    代码优化

--- langChain/DeepSeekChat.py
+++ langChain/DeepSeekChat.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage
 from secret_utils import decrypt_env_value
 import httpx
 import os
@@ -7,9 +8,10 @@ import os
 load_dotenv()
 
 encrypted_api_key = os.getenv("DEEPSEEK_API_KEY_ENCRYPTED")
-api_key_password = ""
+api_key_password = os.environ["DEEPSEEK_API_KEY_PASSWORD"]
 api_key = decrypt_env_value(encrypted_api_key, api_key_password)
 
+# 对话模型
 llm = ChatOpenAI(
     model="deepseek-v4-flash",
     api_key=api_key,
@@ -20,5 +22,11 @@ llm = ChatOpenAI(
     http_socket_options=(),
 )
 
-response = llm.invoke("你当前使用的是什么模型，确定我的用量信息，账户中已经花费了多少钱")
+messages = [
+    SystemMessage(content="你是一个诗人"),
+    HumanMessage(content="写一首关于夏天的诗")
+]
+response = llm.invoke(messages)
+# AIMessage
+print(type(response))
 print(response.content)
